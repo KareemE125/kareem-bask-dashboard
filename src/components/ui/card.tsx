6a79fcf465fafa6e7813e13519c93469eb91c0d1
KareemE125 <kareemezzat.125@gmail.com>
2024-08-08 00:55:52 +0300

    Add missing height property to Card component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-md",
+      "h-full rounded-lg border bg-card text-card-foreground shadow-md",
       className
     )}
     {...props}
